39fcb8b0e13c60491b769b874773fc34b59b25d0
Ben Francisco <bfrancisco.home@gmail.com>
2018-11-01 13:18:17 -0500

    initial commit

---
+++ AD-Install.ps1
@@ -0,0 +1,6 @@
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Get-WindowsFeature AD-Domain-Services | Install-WindowsFeature -source wim:d:\sources\install.wim:2
+$pwd = ConvertTo-SecureString -String 'Francisco1' -AsPlainText -Force
+import-Module ADDSDeployment 
+Install-ADDSForest -DomainName "corp.francisco.science" -DomainNetBiosName  "corp" -DomainMode 6 -ForestMode 6 -NoDnsOnNetwork -NoRebootOnCompletion -safemodeadministratorpassword $pwd -force
+Restart-Computer
