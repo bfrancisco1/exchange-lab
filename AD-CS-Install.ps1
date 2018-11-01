043519ba5115539021b234a873a07a63eaa7dbca
Benjamin Francisco <bfrancisco@paylocity.com>
2018-11-01 14:21:12 -0500

    added notes for manual config of CA

--- AD-CS-Install.ps1
+++ AD-CS-Install.ps1
@@ -4,8 +4,18 @@ Add-WindowsFeature Adcs-Enroll-Web-Svc -IncludeAllSubFeature -IncludeManagementT
 Add-WindowsFeature Adcs-Web-Enrollment -IncludeAllSubFeature -IncludeManagementTools
 
 #Configure the certificate service via ServerManager
-#SHA256 Hash algorithm
+##Select Certification Authority and Certification Authority Web Enrollment
+##Select ENterprise CA
+##Select ROot CA
+##Create new private key
+##SHA256 Hash algorithm
+##Allow admin interaction
+##leave default names
+##specify 5 year validity
+##leave default database location
 #
-#For web Enroll
-#windows type authentication
-#built-in identity pool
+#For cert Enrollweb service
+##select CA name
+##windows type authentication
+##built-in identity pool
+##use default SSL cert (this is the root cert for the root CA)
