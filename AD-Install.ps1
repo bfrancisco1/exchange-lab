64d1f8d84a5e6b87c47189f97622aad87fa2b773
Benjamin Francisco <bfrancisco@paylocity.com>
2018-11-01 14:20:54 -0500

    added RSAT tools install

--- AD-Install.ps1
+++ AD-Install.ps1
@@ -1,6 +1,7 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Add-WindowsFeature RSAT-AD-Tools
 Get-WindowsFeature AD-Domain-Services | Install-WindowsFeature -source wim:d:\sources\install.wim:2
 $pwd = ConvertTo-SecureString -String 'Francisco1' -AsPlainText -Force
 import-Module ADDSDeployment 
-Install-ADDSForest -DomainName "corp.francisco.science" -DomainNetBiosName  "corp" -DomainMode 6 -ForestMode 6 -NoDnsOnNetwork -NoRebootOnCompletion -safemodeadministratorpassword $pwd -force
+Install-ADDSForest -DomainName "corp.francisco.science" -DomainNetBiosName  "corp" -DomainMode 6 -ForestMode 6 -NoDnsOnNetwork -NoRebootOnCompletion -safemodeadministratorpassword $pwd -force -includemanagementtools
 Restart-Computer
