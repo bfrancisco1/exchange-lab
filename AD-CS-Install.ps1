1736a29dbce85bbb9d7671212a38cdd613446ac6
Ben Francisco <bfrancisco.home@gmail.com>
2018-11-01 13:53:39 -0500

    renamed

---
+++ AD-CS-Install.ps1
@@ -0,0 +1,11 @@
+import-module ServerManager
+Add-WindowsFeature Adcs-Cert-Authority -IncludeManagementTools
+Add-WindowsFeature Adcs-Enroll-Web-Svc -IncludeAllSubFeature -IncludeManagementTools
+Add-WindowsFeature Adcs-Web-Enrollment -IncludeAllSubFeature -IncludeManagementTools
+
+Configure the certificate service via ServerManager
+SHA256 Hash algorithm
+
+For web Enroll
+windows type authentication
+built-in identity pool
