8714adfc42a053101f1124c7d95e04de506215ad
Benjamin Francisco <bfrancisco@paylocity.com>
2018-11-03 20:17:30 -0500

    removed management tools include

--- AD-Install.ps1
+++ AD-Install.ps1
@@ -3,5 +3,5 @@ Add-WindowsFeature RSAT-AD-Tools
 Get-WindowsFeature AD-Domain-Services | Install-WindowsFeature -source wim:d:\sources\install.wim:2
 $pwd = ConvertTo-SecureString -String 'Francisco1' -AsPlainText -Force
 import-Module ADDSDeployment 
-Install-ADDSForest -DomainName "corp.francisco.science" -DomainNetBiosName  "corp" -DomainMode 6 -ForestMode 6 -NoDnsOnNetwork -NoRebootOnCompletion -safemodeadministratorpassword $pwd -force -includemanagementtools
+Install-ADDSForest -DomainName "corp.francisco.science" -DomainNetBiosName  "corp" -DomainMode 6 -ForestMode 6 -NoDnsOnNetwork -NoRebootOnCompletion -safemodeadministratorpassword $pwd -force
 Restart-Computer
